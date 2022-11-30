b359b64b402ad5c4d0e0324484d1bc4feb9b842a
Ash4gold <104778976+Ash4gold@users.noreply.github.com>
2022-11-30 12:42:47 -0900

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,18 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char c;
-
-for (c = '0'; c <= '9'; c++)
-
-putchar(c);
-
-putchar('\n');
-return (0);
-}
+#!/usr/bin/python3
+for i in range(0, 99):
+    print("{:02d}".format(i), end=', ')
+print("{:02d}".format(i + 1))
